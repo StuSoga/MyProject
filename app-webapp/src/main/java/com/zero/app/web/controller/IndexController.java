c291d8167a3cc2be9ac4d73255a19f648ca88993
zero <jinzhaoxike@163.com>
2016-02-29 16:58:18 +0800

    修改

--- app-webapp/src/main/java/com/zero/app/web/controller/IndexController.java
+++ app-webapp/src/main/java/com/zero/app/web/controller/IndexController.java
@@ -2,6 +2,8 @@ package com.zero.app.web.controller;
 
 import com.zero.constants.RespCode;
 import com.zero.constants.RespMSG;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,12 +14,15 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class IndexController {
 
+	private Logger LOG= LoggerFactory.getLogger(IndexController.class);
+
 	@RequestMapping(value = {"","/"})
 	public ModelAndView index() {
 
 		ModelAndView view=new ModelAndView("/index");
 		view.addObject("success", RespCode.SUCCESS);
 		view.addObject("message", RespMSG.SUCCESS);
+		LOG.info("/index");
 		return view;
 	}
 }
